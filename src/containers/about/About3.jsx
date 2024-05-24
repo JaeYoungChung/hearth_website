3ed873e4203bf82985fbb28c31be4bd670465bdb
Chung Jae Young <smiggle991227@gmail.com>
2024-05-24 16:25:01 +0900

    responsive

--- src/containers/about/About3.jsx
+++ src/containers/about/About3.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './about.css';
+import './about.css'; 
 import { Route } from 'react-router-dom';
 import { t } from 'i18next';
 import { useTranslation } from 'react-i18next';
