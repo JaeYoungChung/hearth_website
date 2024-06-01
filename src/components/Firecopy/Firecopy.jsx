76e4a0cf7cdd7d2ba88ee210236fd0f12b6fa753
Chung Jae Young <smiggle991227@gmail.com>
2024-06-01 16:56:27 +0900

    some changes

--- src/components/Firecopy/Firecopy.jsx
+++ src/components/Firecopy/Firecopy.jsx
@@ -806,57 +806,57 @@ const Firecopy = () => {
             <img src={rotateLeft} className="arrow left-arrow" ref={leftArrowRef} onClick={rotateClockwise}></img>
             <img src={rotateRight} className="arrow right-arrow" ref={rightArrowRef} onClick={rotateCounterClockwise}></img>
           <div className={tiltedsvg ? 'tilted-svg' : ''}>
-          <div className="svg-wrapper">
-          <div
-            className={`f-svg-container ${showContent ? 'hidden' : ''} ${rotation !== 0 ? 'rotate' : ''}`}
-            ref={svgRef}
-            style={{ transform: rotationButton !== 0 ? `perspective(1000px) rotate3d(0, -2.747, 1, ${rotation}deg)` : null }}
-          >
-              <svg
-                className={tilted ? (rotationButton !== 0) ? 'f-tilted' : 'f-tilted-reset' :''}
-                style={{ transform: rotationButton === 0 ? `translate(0px, -350px) scale(70%) rotate3d(0, 0, 1, ${mobileRotation}deg)` : null, overflow: 'visible'}}
+            <div className="svg-wrapper">
+              <div
+                className={`f-svg-container ${showContent ? 'hidden' : ''} ${rotation !== 0 ? 'rotate' : ''}`}
+                ref={svgRef}
+                style={{ transform: rotationButton !== 0 ? `perspective(1000px) rotate3d(0, -2.747, 1, ${rotation}deg)` : null }}
               >
-                {/* Outer grey hexagon */}
-                <polygon
-                points={outerHexagonPoints.map(p => `${p.x},${p.y}`).join(" ")}
-                stroke={hexagonColor}
-                strokeWidth="2"
-                fill= "#1a1a1a"
-                />
-                {/* Inner shape */}
-                <polygon
-                points={innerHexagonPoints.map(p => `${p.x},${p.y}`).join(" ")}
-                fill={hexagonColor}
-                stroke="darkgrey"
-                strokeWidth="2"
-                />
-                {/* Draw lines connecting opposite */}
-                <line x1={outerHexagonPoints[0].x} y1={outerHexagonPoints[0].y} x2={outerHexagonPoints[3].x} y2={outerHexagonPoints[3].y} stroke={hexagonColor} />
-                <line x1={outerHexagonPoints[1].x} y1={outerHexagonPoints[1].y} x2={outerHexagonPoints[4].x} y2={outerHexagonPoints[4].y} stroke={hexagonColor} />
-                <line x1={outerHexagonPoints[2].x} y1={outerHexagonPoints[2].y} x2={outerHexagonPoints[5].x} y2={outerHexagonPoints[5].y} stroke={hexagonColor} />
-                {/* labels */}
-                {
-                labelHexagonPoints.map((point, i) => (
-                    <text
-                        width={30}
-                        className='label-text'
-                        x={point.x} 
-                        y={point.y} 
-                        fontSize="30"
-                        fill={changeTextColor ? colors[i] : "white"}
-                        key={i}
-                        textAnchor="middle"
-                        dominantBaseline="middle"
-                        transform={`translate(${point.x} ${point.y}) 
-                            rotate(${-rotation + textRotationAdjustment -2}) 
-                            translate(${-point.x} ${-point.y})`}
-                    >
-                        {hexagon_labels[i]}
-                    </text>
-                ))
-                }
-            </svg>
-            </div>
+                <svg
+                  className={tilted ? (rotationButton !== 0) ? 'f-tilted' : 'f-tilted-reset' :''}
+                  style={{ transform: rotationButton === 0 ? `translate(0px, -350px) scale(70%) rotate3d(0, 0, 1, ${mobileRotation}deg)` : null, overflow: 'visible'}}
+                >
+                  {/* Outer grey hexagon */}
+                  <polygon
+                  points={outerHexagonPoints.map(p => `${p.x},${p.y}`).join(" ")}
+                  stroke={hexagonColor}
+                  strokeWidth="2"
+                  fill= "#1a1a1a"
+                  />
+                  {/* Inner shape */}
+                  <polygon
+                  points={innerHexagonPoints.map(p => `${p.x},${p.y}`).join(" ")}
+                  fill={hexagonColor}
+                  stroke="darkgrey"
+                  strokeWidth="2"
+                  />
+                  {/* Draw lines connecting opposite */}
+                  <line x1={outerHexagonPoints[0].x} y1={outerHexagonPoints[0].y} x2={outerHexagonPoints[3].x} y2={outerHexagonPoints[3].y} stroke={hexagonColor} />
+                  <line x1={outerHexagonPoints[1].x} y1={outerHexagonPoints[1].y} x2={outerHexagonPoints[4].x} y2={outerHexagonPoints[4].y} stroke={hexagonColor} />
+                  <line x1={outerHexagonPoints[2].x} y1={outerHexagonPoints[2].y} x2={outerHexagonPoints[5].x} y2={outerHexagonPoints[5].y} stroke={hexagonColor} />
+                  {/* labels */}
+                  {
+                  labelHexagonPoints.map((point, i) => (
+                      <text
+                          width={30}
+                          className='label-text'
+                          x={point.x} 
+                          y={point.y} 
+                          fontSize="30"
+                          fill={changeTextColor ? colors[i] : "white"}
+                          key={i}
+                          textAnchor="middle"
+                          dominantBaseline="middle"
+                          transform={`translate(${point.x} ${point.y}) 
+                              rotate(${-rotation + textRotationAdjustment -2}) 
+                              translate(${-point.x} ${-point.y})`}
+                      >
+                          {hexagon_labels[i]}
+                      </text>
+                  ))
+                  }
+                </svg>
+              </div>
             </div>
           </div>
         <div className="text-left" ref={textLeftRef}>
