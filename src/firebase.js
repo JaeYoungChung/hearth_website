ceb771ec9a651865e42d1b0fae9aab6874722910
Chung Jae Young <smiggle991227@gmail.com>
2024-06-04 13:15:12 +0900

    added env

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getAnalytics } from "firebase/analytics";
 import {getDatabase} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB31K2STIfBh2RNHT0YzKRa4mN-2gFO5Rk",
-  authDomain: "hearthwebsite-7047d.firebaseapp.com",
-  databaseURL: "https://hearthwebsite-7047d-default-rtdb.firebaseio.com",
-  projectId: "hearthwebsite-7047d",
-  storageBucket: "hearthwebsite-7047d.appspot.com",
-  messagingSenderId: "715450515037",
-  appId: "1:715450515037:web:c5ddf2b6362c858f2f57b4",
-  measurementId: "G-MZE54YVG8G"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 
