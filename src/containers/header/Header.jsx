256ae67f96357c1d713d6c1b9959eb4eb7b065c7
Chung Jae Young <smiggle991227@gmail.com>
2024-10-30 10:25:50 +0900

    edited og tag

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -52,6 +52,10 @@ const Header = memo(() => {
           <title>HEARTH: Test Your Cognitive Force & Improve Yourself</title>
           <meta name="description" content="Hearth is here to help. Know yourself better and utilize the power of mind. With the HEARTH Apps, walk your continuous journey of self-improvement." />
           <meta name="robots" content="index, follow"></meta>
+          <meta property="og:site_name" content="HEARTH" />
+          <meta property="og:title" content="HEARTH: Test Your Cognitive Force & Improve Yourself" />
+          <meta property="og:url" content="www.hearthishere.com" />
+          <meta property="og:description" content="Hearth is here to help. Know yourself better and utilize the power of mind. With the HEARTH Apps, walk your continuous journey of self-improvement." />
         </Helmet>
       <div className='header-content'>
         {/* Priority LCP element */}
